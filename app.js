637c29f6b3f8bc92640d337871f4ad4334df2543
Cesar Castillo <cesarca378@gmail.com>
2022-10-03 22:41:20 -0500

    server started

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const express =  require('express');
 const app = express()
 const router = require('router')
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use(router)
+
+app.listen(3000, (error) => {
+    error ? console.log(error) : 
+    console.log("Server started on port 3000")
+})
