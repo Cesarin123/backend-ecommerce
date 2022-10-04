00513f8cc9535951cc3c255d45ee8a77af91b8b6
Cesar Castillo <cesarca378@gmail.com>
2022-10-04 00:32:06 -0500

    product controllers and route structure updated

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express =  require('express')
+
 const app = express()
+
 const router = require('./routes/products')
 
 /* Middlewares --> get data from request before getting
 to the function */
 app.use(express.json()) /* Allow communication thru JSON format */
+
 app.use(express.urlencoded({extended: false}))
+
 
 app.use(router)
 
+
 /* Server startup on port 3000*/
 app.listen(3000, (error) => {
+
     error ? console.log(error) : 
+
     console.log("Server started on port 3000")
+
 })
